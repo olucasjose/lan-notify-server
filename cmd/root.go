5de517d4d766bf6b22cf023527959d1f7edb564b
olucasjose <65680721+olucasjose@users.noreply.github.com>
2026-05-22 20:44:17 -0300

    feat: add base cobra cli structure

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "lan-notify",
+	Short: "LAN Notify is a local network notification system",
+	Long:  `A lightweight background service and CLI client to send and receive desktop notifications over the local network using mDNS and HTTPS.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"lan-notify/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
